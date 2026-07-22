26dcfa7c5a1d8c5e90745deac6b42239e739b679
Zumhadik <nikitad54367@gmail.com>
2026-07-22 04:42:57 +0300

    Delete UnboundMacro directory

--- UnboundMacro/resources/resource.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define IDR_FONT_SFPRO 101
-#define IDI_APPICON 102
--- UnboundMacro/src/macro/MacroFile.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-class MacroFile {
-public:
-    static bool CreateNew(std::wstring* out_path);
-    static bool OpenExisting(std::wstring* out_path);
-};
